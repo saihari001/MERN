5dcc1fb7c0f4f090d34ef105f9e74bc3f304f8ca
Hari Prasanth <71781116+saihari001@users.noreply.github.com>
2024-03-14 13:07:06 +0530

    Update authRoutes.js
    added deploy link in cors

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -8,7 +8,7 @@ const { test,registerUser,loginUser,logout, profile, forgotPassword, resetPasswo
 router.use(
     cors({
         credentials: true,
-        origin: 'http://localhost:5173'
+        origin: ['http://localhost:5173', 'https://mern-authentication-23df.onrender.com'],
     })
 )
 
